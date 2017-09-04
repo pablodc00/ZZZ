d87b80e31281926f6c292fb56dc95da0b4270e57
Pablo DC <pablo.dcristofaro@globant.com>
2017-09-03 22:54:07 -0300

    NA

--- src/main/java/com/dummy/work/AppCompute.java
+++ src/main/java/com/dummy/work/AppCompute.java
@@ -6,6 +6,16 @@ import java.util.Map;
 public class AppCompute {
 
     public static void main(String[] args) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int a=1; a<=10; a++) {
+            map.putIfAbsent(a, 0);
+            map.computeIfPresent(5, (num, val) -> val + 1);
+        }
+        
+        
+        
         Map<String, Integer> dummy = new HashMap<>();
         dummy.compute("one", (k, v) -> v == null ? 1 : v + 1);
         dummy.compute("one", (k, v) -> v == null ? 1 : v + 1);
@@ -19,7 +29,7 @@ public class AppCompute {
         System.out.println(dummy.getOrDefault("four", 0));
         System.out.println(dummy.getOrDefault("one", 0));
         
-        
+
     }
 
 }
--- src/main/java/com/dummy/work/Dummy.java
+++ src/main/java/com/dummy/work/Dummy.java
@@ -5,15 +5,19 @@ import java.util.Date;
 
 public class Dummy {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-		
-		long date = new Date().getTime();
-		System.out.println(date);
-		
-		LocalDate localDate = LocalDate.parse("nada");
-		System.out.println(localDate);
-	}
+        Double dou1 = 10D;
+        Double dou2 = 15D;
+        
+        Double result = dou1 * dou2;
+        System.out.println(result);
+        
+        long date = new Date().getTime();
+        System.out.println(date);
+
+        LocalDate localDate = LocalDate.parse("nada");
+        System.out.println(localDate);
+    }
 
 }
