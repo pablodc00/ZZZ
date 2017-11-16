b4037e306d79e55753badc93967424d313e447f3
Pablo DC <pablo.dcristofaro@globant.com>
2017-11-16 13:48:55 -0300

    NA

--- src/main/java/com/dummy/work/MainString3.java
+++ src/main/java/com/dummy/work/MainString3.java
@@ -13,5 +13,16 @@ public class MainString3 {
         System.out.println(sequence.substring(0, size));
         System.out.println(sequence.substring(size -1, size));
 
+        String s1 = new String( "Test" );
+        String s2 = new String( "Test" );
+        
+        
+        System.out.println( s1 == s2 ); // false
+        System.out.println( s1.equals( s2 )); // true
+        
+        
+        System.out.println("END");
+        
+        
     }
 }
